6dfc0573220b18809fc699fe6e7fdb16f1419374
Paul Al-Dabagh <paul.al-dabagh@hva.nl>
2022-11-12 15:28:20 +0100

    added linting

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,21 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 describe('AppController', () => {
-  let app: TestingModule;
+  let app: TestingModule
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
-    }).compile();
-  });
+    }).compile()
+  })
 
   describe('getHello', () => {
     it('should return "Lets start!"', () => {
-      const appController = app.get(AppController);
-      expect(appController.getHello()).toBe('Lets start!');
-    });
-  });
-});
+      const appController = app.get(AppController)
+      expect(appController.getHello()).toBe('Lets start!')
+    })
+  })
+})
--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 const App = (): JSX.Element => {
   return (
@@ -7,16 +7,16 @@ const App = (): JSX.Element => {
       <Text>Lees de readme in de root folder!</Text>
       <StatusBar style="auto" />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+})
 
-export default App;
+export default App
