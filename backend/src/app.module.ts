cf86a0299fe4d2e6fa751680cfd793a0fcd6c419
Paul Aldabagh <paulaldabagh@re-byods-145-28-148-223.wireless.hva.nl>
2022-11-25 11:40:01 +0100

    fixed linting error

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,13 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {HttpModule} from "@nestjs/axios";
-import {UserProfileController} from "./userProfile/UserProfile.controller";
-import {UserProfileService} from "./userProfile/UserProfile.service";
-
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { HttpModule } from '@nestjs/axios'
+import { UserProfileController } from './userProfile/UserProfile.controller'
+import { UserProfileService } from './userProfile/UserProfile.service'
 
 @Module({
-    imports: [HttpModule],
-    controllers: [AppController, UserProfileController],
-    providers: [AppService, UserProfileService],
+  imports: [HttpModule],
+  controllers: [AppController, UserProfileController],
+  providers: [AppService, UserProfileService],
 })
-export class AppModule {
-}
+export class AppModule {}
