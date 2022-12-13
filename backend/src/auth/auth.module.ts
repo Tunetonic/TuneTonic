0f88e7e6ed4fb3204b09583bbdb5badf68f9282b
Hessel Koffeman <hessel.koffeman@hva.nl>
2022-12-13 14:44:16 +0100

    feat: added JWT passport strategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { UserModule } from 'src/user/user.module'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.jwtSecretKey,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common'
+import { User } from 'src/user/user.entity'
 import { UserService } from 'src/user/user.service'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async createToken(user: User) {
+    const payload = {
+      sub: user.id,
+      role: user.role,
+    }
+    return {
+      access_token: this.jwtService.sign(payload),
+    }
+  }
 }
