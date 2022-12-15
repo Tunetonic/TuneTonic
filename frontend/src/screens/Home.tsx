1b6f25f6e727b2167bf8731206f99013027dd7f7
jabbarn <nomanjabbar1999@gmail.com>
2022-12-15 20:27:33 +0100

    feat(home): implement scroll + playback-dock

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,104 +1,58 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ScrollView, Image, StyleSheet, Dimensions, Platform, View } from 'react-native'
-import { Card, IconButton, Text, Title } from 'react-native-paper'
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react'
+import { Image, StyleSheet, Dimensions, Platform, View, FlatList, TouchableHighlight } from 'react-native'
+import { Card, IconButton, Text } from 'react-native-paper'
 import { getPlaylist } from '../services/user.service'
 import { Track, trackItemMapper } from '../util/track'
-import { Audio } from 'expo-av';
-import { Sound } from 'expo-av/build/Audio'
+import { Audio, AVPlaybackStatus } from 'expo-av';
+import { SoundObject } from 'expo-av/build/Audio'
+import Slider from '@react-native-community/slider'
+import { themeContext } from '../providers/theme.provider'
+import { millisToHHMMSS } from '../../helpers'
 
 const Home = ({ navigation }): JSX.Element => {
-
+  const { theme } = useContext(themeContext) 
+  const [selectedTrack, setSelectedTrack] = useState<Track>();
   const [tracks, setTracks] = useState<Track[]>([])
-  const [sound, setSound] = useState<Sound>()
-  // let tracks = [
-  //   {
-  //     id: '1',
-  //   },
-  //   {
-  //     id: '2',
-  //   },
-  //   {
-  //     id: '3',
-  //   },
-  //   {
-  //     id: '4',
-  //   },
-  //   {
-  //     id: '5',
-  //   },
-  //   {
-  //     id: '6',
-  //   },
-  //   {
-  //     id: '7',
-  //   },
-  //   {
-  //     id: '8',
-  //   },
-  //   {
-  //     id: '9',
-  //   },
-  //   {
-  //     id: '11',
-  //   },
-  //   {
-  //     id: '12',
-  //   },
-  //   {
-  //     id: '13',
-  //   },
-  //   {
-  //     id: '14',
-  //   },
-  //   {
-  //     id: '15',
-  //   },
-  //   {
-  //     id: '16',
-  //   },
-  //   {
-  //     id: '17',
-  //   },
-  //   {
-  //     id: '18',
-  //   },
-  //   {
-  //     id: '19',
-  //   },
-  // ]
-
-  const playAudio = async (src: string | undefined) => {
-    if(src) {
-      console.log('Loading Sound: ', src)
-      const { sound } = await Audio.Sound.createAsync({uri: src})
-      setSound(sound)
+  const audioSoundRef = React.useRef(new Audio.Sound());
+  const [soundStatus, setSoundStatus] = useState<AVPlaybackStatus>()
   
-      console.log('Playing Sound: ', src)
-      await sound.playAsync()
-    }
-    else {
-      console.log("too bad kid, no preview_url on this track.")
-    }
-  }
+
+  // const onPlaybackStatusUpdate = async (playbackStatus) => {
+  //   await setSound(playbackStatus);
+  //   if (playbackStatus.isPlaying) {
+  //   }
+  // }
+  // const playAudio = async (src: string | undefined) => {
+  //   if(src) {
+  //     const { sound } = await Audio.Sound.createAsync({uri: src})
+  //     setSound(sound)
+  
+  //     console.log('Playing Sound: ', src)
+  //     await sound.playAsync()
+  //   }
+  //   else {
+  //     console.log("too bad kid, no preview_url on this track.")
+  //   }
+  // }
 
   useEffect(() => {
-    getPlaylist("4Mu1GSMPiyxDphkPeGJDJT").then((data) => {
+    getPlaylist("1LwKg8pkx71G83WgOfvlLZ").then((data) => {
       setTracks(trackItemMapper(data.items))
     })
   }, [])
 
-  useEffect(() => {
-    return sound ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync();
-        }
-      : console.log("hit?");
-  }, [sound]);
+  // useEffect(() => {
+  //   return sound ? () => {
+  //         console.log('Unloading Sound');
+  //         sound.unloadAsync();
+  //       }
+  //     : console.log("hit?");
+  // }, [sound]);
 
-  
+  const BOTTOM_TAB_HEIGHT = 77.71
+  const PLAYBACK_HEIGHT = 40
   const CARD_WIDTH = Dimensions.get('window').width
-  const CARD_HEIGHT = Dimensions.get('window').height - 90
-  const SPACING_FOR_CARD_INSET = 5
+  const CARD_HEIGHT = Dimensions.get('window').height - BOTTOM_TAB_HEIGHT - PLAYBACK_HEIGHT
   const styles = StyleSheet.create({
     cardContentStyle: {
       flex: 1,
@@ -111,9 +65,7 @@ const Home = ({ navigation }): JSX.Element => {
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: 'navy',
-      color: 'white',
-      marginVertical: 5,
+      // backgroundColor: 'teal',
       borderRadius: 15
     },
     title: {
@@ -137,42 +89,110 @@ const Home = ({ navigation }): JSX.Element => {
       marginHorizontal: 10,
     },
   })
+  const onViewableItemsChanged = useCallback( ({ viewableItems }) => { 
 
-  return (
-    <ScrollView
-      decelerationRate={0.3}
-      snapToInterval={CARD_HEIGHT + 12}
+    if (viewableItems.length > 0) {
+      setSelectedTrack(viewableItems[0]['item'])
+
+      if(viewableItems[0]['item']['preview_url']) {
+        Audio.Sound.createAsync({uri: viewableItems[0]['item']['preview_url']},
+          {
+          shouldPlay: false,
+          isLooping: false,
+          }
+        ).then(({ sound }) => {
+          console.log("soundObj: ", sound)
+          audioSoundRef.current = sound
+
+          audioSoundRef.current.getStatusAsync()
+          .then((status) => {
+            console.log("status: ", status)
+            setSoundStatus(status)
+          })
+        })
+        console.log("hit!?")
+      } else {
+        console.log("too bad kid, theres no preview for this track.")
+      }
+      
+    }
+  }, [])
+
+  useEffect(() => {
+    if(selectedTrack?.preview_url) {
+      audioSoundRef.current.getStatusAsync()
+      .then(status => setSoundStatus(status))
+    }    
+  }, [selectedTrack])
+
+  const _viewabilityConfig = useRef({
+    minimumViewTime: 200, 
+    itemVisiblePercentThreshold: 50
+  })
+
+  return (<>
+    <FlatList
+      decelerationRate={0.7}
+      snapToInterval={CARD_HEIGHT}
+      keyExtractor={item => item.id}
+      showsVerticalScrollIndicator={false}
+      viewabilityConfig={_viewabilityConfig.current}
+      onViewableItemsChanged={onViewableItemsChanged}
       contentInset={{
-        top: SPACING_FOR_CARD_INSET,
+        top: 0,
         left: 0,
-        bottom: SPACING_FOR_CARD_INSET,
+        bottom: 0,
         right: 0
       }}
-      snapToAlignment="center"
+      snapToAlignment="start"
       contentContainerStyle={{
-        paddingVertical: Platform.OS === 'android' ? SPACING_FOR_CARD_INSET : 0
-      }}>
-        {tracks.length > 0 &&
-          tracks.map((track) => {
-            return (
-              <Card key={track.id} style={styles.cardStyle}>
-                <Card.Content style={styles.cardContentStyle}>
-                  <Image
-                    source={{ uri: track.image }}
-                    style={styles.image}
-                    />
-                  <Title>{track.name}</Title>
-                  <IconButton
-                    icon='play-circle'
-                    color='#1ED760'
-                    size={50}
-                    onPress={ async () => await playAudio(track.preview_url)}
-                  />
-                </Card.Content>
-              </Card>
-            )
-          })}
-    </ScrollView>
-  )
+        paddingVertical: Platform.OS === 'android' ? 0 : 0
+      }}
+      data={tracks}
+      // extraData={[soundStatus]}
+      renderItem={({ item, index }) => (
+        <TouchableHighlight
+          key={item.id}
+          >
+            <Card key={item.id} style={styles.cardStyle}>
+              <Card.Content style={styles.cardContentStyle}>
+                <Image
+                  source={{ uri: item.image }}
+                  style={styles.image}
+                />
+                {/* <Text style={styles.title}>{item.id} | {CARD_HEIGHT} | {index}</Text> */}
+              </Card.Content>
+            </Card>
+          </TouchableHighlight>
+        )}
+      />
+    <View style={{ 'margin': 10 }}>
+      <View style={{display:'flex', 'justifyContent': 'space-between', 'flexDirection': 'row'}}>
+        <View style={{'flexDirection': 'column'}}>
+          <Text>{selectedTrack ? selectedTrack.name : ''}</Text>
+          <Text style={{ color: 'rgba(255,255,255, 0.6)'}}>{selectedTrack ? 'by '  + selectedTrack.artist_name: ''}</Text>
+        </View>
+        <IconButton icon={'play-circle-outline'}></IconButton>
+
+        {/* <View>
+          {/* <IconButton icon={'pause-circle-outline'}></IconButton>
+        </View> */}
+      </View>
+
+
+      <Slider
+        style={{width: CARD_WIDTH - 20, height: 40}}
+        minimumValue={ soundStatus && soundStatus.isLoaded ? soundStatus?.positionMillis : 0}
+        maximumValue={ soundStatus && soundStatus.isLoaded ? soundStatus?.durationMillis : 1}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#DBDBDB"
+        thumbTintColor={theme.colors.primary}
+      />
+      <View style={{ 'display': 'flex', 'justifyContent': 'space-between', 'flexDirection': 'row'}}>
+        <Text style={{'fontSize': 11}}>{soundStatus && soundStatus.isLoaded && selectedTrack?.preview_url ? millisToHHMMSS(soundStatus?.positionMillis) : '--:--'}</Text>
+        <Text style={{'fontSize': 11}}>{soundStatus && soundStatus.isLoaded && soundStatus.durationMillis && selectedTrack?.preview_url ? millisToHHMMSS(soundStatus?.durationMillis): '--:--'}</Text>
+      </View>
+     </View>
+  </>)
 }
 export default Home
--- frontend/src/util/track.ts
+++ frontend/src/util/track.ts
@@ -1,11 +1,14 @@
 export interface Track {
-  uri: string,
-  type: string,
-  track_number: number,
-  preview_url?: string,
-  name: string,
-  id: string,
+  uri: string
+  type: string
+  track_number: number
+  preview_url?: string
+  name: string
+  id: string
   image: string
+  // album_name: string
+  artist_name: string
+  duration_ms: number
 }
 
 export const trackItemMapper = (arr: any[]): Track[] => {
@@ -16,6 +19,9 @@ export const trackItemMapper = (arr: any[]): Track[] => {
     preview_url: item.track.preview_url,
     name: item.track.name,
     image: item.track.album.images[0].url,
-    id: item.track.id
+    // album_name: item.track.album.name,
+    artist_name: item.track.artists[0].name,
+    id: item.track.id,
+    duration_ms: item.track.duration_ms
   }))
 }
