2c3983579fc1e247e98ffe977fe3e01184189cc6
Hessel Koffeman <hessel.koffeman@hva.nl>
2023-01-25 10:29:21 +0100

    fixed auth test errors

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,25 +1,39 @@
-// import { HttpModule } from '@nestjs/axios'
-// import { JwtService } from '@nestjs/jwt'
-// import { Test, TestingModule } from '@nestjs/testing'
-// import { AuthController } from './auth.controller'
-// import { AuthService } from './auth.service'
+import { HttpModule } from '@nestjs/axios'
+import { JwtService } from '@nestjs/jwt'
+import { Test, TestingModule } from '@nestjs/testing'
+import { User } from '../user/user.entity'
+import { Repository } from 'typeorm'
+import { UserService } from '../user/user.service'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { SpotifyService } from '../spotify/spotify.service'
 
 describe('AuthController', () => {
-  // let controller: AuthController
+  let controller: AuthController
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     imports: [HttpModule],
-  //     providers: [AuthService, JwtService],
-  //     controllers: [AuthController],
-  //   }).compile()
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
+      providers: [
+        AuthService,
+        JwtService,
+        SpotifyService,
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
+      controllers: [AuthController],
+    }).compile()
 
-  //   controller = module.get<AuthController>(AuthController)
-  // })
+    controller = module.get<AuthController>(AuthController)
+  })
 
-  // it('should be defined', () => {
-  //   expect(controller).toBeDefined()
-  // })
+  it('should be defined', () => {
+    expect(controller).toBeDefined()
+  })
   it('test authController', () => {
     expect(true).toBe(true)
   })
