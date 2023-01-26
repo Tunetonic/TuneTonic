2b43ed9dacadd34fdef04ca664478c2bea70be7c
imadkb7 <imad.baktit@hva.nl>
2023-01-26 12:02:47 +0100

    Add test

--- backend/src/likes/likes.controller.ts
+++ backend/src/likes/likes.controller.ts
@@ -20,29 +20,29 @@ export class LikesController {
     @Param('id') itemId: string,
     @Body() likeDetails: LikeDTO,
     @Headers('Authorization') authToken,
-    @Response() res,
+    // @Response() res,
   ) {
     const like: Like = await this.likeService.add(
       likeDetails,
       authToken,
       itemId,
     )
-    res.status(HttpStatus.OK).send({
+
+  return {
       message: 'Successfully liked',
       data: like.genres.map((data) => data.tagName),
-    })
+    }
   }
 
   @Post('dislike')
   async dislike(
     @Body() likeObj: LikeDTO,
-    @Response() res,
     @Headers('Authorization') authToken,
   ) {
     const like = await this.likeService.deleteLike(likeObj, authToken)
-    res.status(HttpStatus.OK).send({
+   return {
       message: 'Successfully disliked',
       data: like,
-    })
+    }
   }
 }
--- frontend/src/util/playlist.util.ts
+++ frontend/src/util/playlist.util.ts
@@ -23,12 +23,3 @@ export const albumItemMapper = (arr: any[]): PlaylistProps[] => {
   }))
 }
 
-export const albumItemMapper = (arr: any[]): PlaylistProps[] => {
-  return arr.map((item) => ({
-    id: item.id,
-    name: item.name,
-    image: item.images[0].url,
-    totalTracks: item.total_tracks,
-  }))
-}
-
